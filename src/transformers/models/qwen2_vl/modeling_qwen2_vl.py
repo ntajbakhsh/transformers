79d9aaabf233b3820f88405c329cac220416aa9a
ntajbakhsh <ntajbakhsh@nvidia.com>
2025-02-09 00:33:29 -0800

    inherited from VLpretained rather than conf gen cls

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -1817,16 +1817,45 @@ class AudioEncoder(nn.Module):
         return self.audio_encoder.encoder(audio)
         #TODO: add adapter layers, post_init() needed?
 
-class Qwen2VLAForConditionalGeneration(Qwen2VLForConditionalGeneration):
+class Qwen2VLAForConditionalGeneration(Qwen2VLPreTrainedModel, GenerationMixin):
+    _tied_weights_keys = ["lm_head.weight"]
+
     def __init__(self, config):
         super().__init__(config)
-        self.config = config
+        self.visual = Qwen2VisionTransformerPretrainedModel._from_config(config.vision_config)
+        self.model = Qwen2VLModel(config)
         self.audio_ecnoder = AudioEncoder()
+
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.rope_deltas = None  # cache rope_deltas here
 
         #TODO: check if resize should be called on self or self.model
         #TODO: check if lm_head is updated too
         self.resize_token_embeddings(self.config.vocab_size + 3) # 3 special tokens for audio
 
+        # Initialize weights and apply final processing
+        self.post_init()
+
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+
+    def set_input_embeddings(self, value):
+        self.model.embed_tokens = value
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def set_output_embeddings(self, new_embeddings):
+        self.lm_head = new_embeddings
+
+    def set_decoder(self, decoder):
+        self.model = decoder
+
+    def get_decoder(self):
+        return self.model
+    
     def get_rope_index(
         self,
         input_ids: Optional[torch.LongTensor] = None,
